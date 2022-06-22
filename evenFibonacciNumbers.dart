5061341701ea79ba8a329c0f42b5e94f1968304f
Luka Lekcevic <luka.lekcevi@gmail.com>
2022-06-22 19:48:53 +0200

    User input for max number, oddfib generator and more general totalFib method

--- evenFibonacciNumbers.dart
+++ evenFibonacciNumbers.dart
@@ -1,43 +1,57 @@
 // By considering the terms in the Fibonacci sequence whose values do not exceed
 // four million, find the sum of the even-valued terms.
 
+import 'dart:io';
+
 class Fibonacci {
   List<int> allFib = [1, 2];
   int index = 2;
   List<int> evenFib = [];
-  int evenFibSum = 0;
-
-  //method that pushes ints in allFib
-  void addAllFib() {
-    while ((allFib[index - 1] + allFib[index - 2]) < 4000000) {
+  List<int> oddFib = [];
+  int FibSum = 0;
+  int max = 0;
+  set setMax(int x) => max = x;
+  int get getMax => max;
+  //method that makes a list of Fibonacci numbers less than x
+  void allFibLessThanX() {
+    while ((allFib[index - 1] + allFib[index - 2]) < max) {
       allFib.add(allFib[index - 1] + allFib[index - 2]);
       index += 1;
     }
   }
 
-  void makeEvenFib() {
+  //method that makes a list of even and odd Fibonacci numbers included in the list allFib
+  void makeOddAndEvenFib() {
     for (int i = 0; i < allFib.length; i++) {
       if (allFib[i] % 2 == 0) {
         evenFib.add(allFib[i]);
+      } else {
+        oddFib.add(allFib[i]);
       }
     }
   }
 
-  void totalEvenFib() {
-    for (int i = 0; i < evenFib.length; i++) {
-      evenFibSum += evenFib[i];
+  //method that adds up all the even Fibonacci numbers in the given list
+  int totalFib(List<int> list) {
+    for (int i = 0; i < list.length; i++) {
+      FibSum += list[i];
     }
+    return 0;
   }
 }
 
 void main() {
   var fib = Fibonacci();
+  print("Input max number");
+  int max = int.parse(stdin.readLineSync()!);
 
-  fib.addAllFib();
-  fib.makeEvenFib();
-  fib.totalEvenFib();
+  fib.setMax = max;
+  fib.allFibLessThanX();
+  fib.makeOddAndEvenFib();
+  fib.totalFib(fib.oddFib);
 
   print("all fib is: ${fib.allFib}");
   print("even fib: ${fib.evenFib}");
-  print("sum of even fibonacci numbers < 4000000: ${fib.evenFibSum}");
+  print("odd fib: ${fib.oddFib}");
+  print("sum of odd fibonacci numbers < $max : ${fib.FibSum}");
 }
